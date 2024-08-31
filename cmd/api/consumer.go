86c14ab83598363a060c8335e5f933c30306fc29
harryd <harry@Wais-MacBook-Pro.local>
2024-08-31 17:59:33 +0700

    Automated commit: Sat Aug 31 17:59:31 +07 2024

--- cmd/api/consumer.go
+++ cmd/api/consumer.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -49,7 +48,6 @@ func (app *application) Cleanup(session sarama.ConsumerGroupSession) error {
 
 func (app *application) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
 	for message := range claim.Messages() {
-		log.Println(string(message.Value))
 		app.processMessage(message)
 		session.MarkMessage(message, "")
 	}
